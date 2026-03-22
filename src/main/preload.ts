4abd87b79973ae8d93d7182e92e441ce8353d188
sdcarter <sdcarter@users.noreply.github.com>
2026-03-22 11:27:22 +0100

    feat: fix overlay chrome and add lock/unlock positioning
    - Add CSS reset to eliminate scrollbars and white bar
    - Add lock/unlock toggle in tray menu (locked by default)
    - Locked: click-through, fixed position, not resizable
    - Unlocked: draggable, resizable, dashed border indicator
    - Content scales with window size via flex layout

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -3,5 +3,6 @@ import { contextBridge, ipcRenderer } from 'electron';
 contextBridge.exposeInMainWorld('electronAPI', {
   onTelemetryUpdate: (callback: (data: any) => void) => ipcRenderer.on('telemetry:update', (_e, data) => callback(data)),
   onTelemetryWaiting: (callback: (msg: string) => void) => ipcRenderer.on('telemetry:waiting', (_e, msg) => callback(msg)),
+  onLockChange: (callback: (locked: boolean) => void) => ipcRenderer.on('overlay:lock', (_e, locked) => callback(locked)),
   setIgnoreMouse: (ignore: boolean) => ipcRenderer.send('set-ignore-mouse', ignore),
 });
